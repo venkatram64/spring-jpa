4904884e8211ed4ac92900044108946830a217b5
venkatram64 <venkat.veerareddy@gmail.com>
2017-08-23 03:38:17 -0400

    init jpa along with spring

---
+++ src/main/java/com/venkat/JpaApplication.java
@@ -0,0 +1,16 @@
+package com.venkat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by venkatram.veerareddy on 8/23/2017.
+ */
+
+@SpringBootApplication
+public class JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaApplication.class, args);
+    }
+}
